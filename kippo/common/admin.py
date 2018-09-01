0d806a0b6e1ec2e68de85802062524f0ff5e77d7
monkut <shane.cousins@gmail.com>
2018-09-01 14:40:31 +0900

    Adding 'StaffRequiredAdminMixin' to allow `is_staff` users access to specific modeladmin items.

--- kippo/common/admin.py
+++ kippo/common/admin.py
@@ -25,6 +25,28 @@ class UserCreatedBaseModelAdmin(admin.ModelAdmin):
         formset.save_m2m()
 
 
+class StaffRequiredAdminMixin(object):
+
+    def check_perm(self, user_obj):
+        if not user_obj.is_active or user_obj.is_anonymous:
+            return False
+        if user_obj.is_superuser or user_obj.is_staff:
+            return True
+        return False
+
+    def has_add_permission(self, request):
+        return self.check_perm(request.user)
+
+    def has_change_permission(self, request, obj=None):
+        return self.check_perm(request.user)
+
+    def has_delete_permission(self, request, obj=None):
+        return self.check_perm(request.user)
+
+    def has_module_permission(self, request, obj=None):
+        return self.check_perm(request.user)
+
+
 class KippoAdminSite(admin.AdminSite):
     # update displayed header/title
     site_header = settings.SITE_HEADER
