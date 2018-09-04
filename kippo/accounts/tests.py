3178202a0478f42411acc57220e7ed60613fcf27
monkut <shane.cousins@gmail.com>
2018-09-04 11:49:22 +0900

    Cleaning up test cases

--- kippo/accounts/tests.py
+++ kippo/accounts/tests.py
@@ -1,25 +1,46 @@
 from django.test import TestCase
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError, PermissionDenied
 from .models import KippoOrganization, KippoUser, EmailDomain
 
 
 class KippoUserCreationTestCase(TestCase):
+    fixtures = [
+        'required_bot_users',
+        'default_columnset',
+        'default_labelset',
+    ]
 
     def setUp(self):
+        self.user = KippoUser(
+            username='accounts-octocat',
+            password='test',
+            email='accounts@github.com',
+            is_staff=True,
+            is_developer=True,
+        )
+        self.user.save(ignore_email_domain_check=True)
+
         self.org = KippoOrganization(name='some org',
-                                     github_organization_name='some-org')
+                                     github_organization_name='some-org',
+                                     created_by=self.user,
+                                     updated_by=self.user,
+                                     )
         self.org.save()
         self.domain = 'kippo.org'
         self.emaildomain = EmailDomain(organization=self.org,
                                        domain=self.domain,
-                                       is_staff_domain=True)
+                                       is_staff_domain=True,
+                                       created_by=self.user,
+                                       updated_by=self.user,
+                                       )
+        self.emaildomain.save()
 
     def test_create_kippouser(self):
         user1 = KippoUser(organization=self.org,
                          username='someuser',
                          email='otheremail@other.com')
-        user1.save()
-        self.assertFalse(user1.is_staff)
+        with self.assertRaises(PermissionDenied):
+            user1.save()
 
         user2 = KippoUser(organization=self.org,
                           username='otheruser',
@@ -28,16 +49,30 @@ class KippoUserCreationTestCase(TestCase):
         self.assertTrue(user2.is_staff)
 
     def test_invalid_emaildomain(self):
+        user = KippoUser(
+            username='octocat',
+            password='test',
+            email='a@github.com',
+            is_staff=True,
+            is_developer=True,
+        )
+        user.save(ignore_email_domain_check=True)
         invalid_email_domain = 'invalid'
         domain = EmailDomain(organization=self.org,
-                             domain=invalid_email_domain)
+                             domain=invalid_email_domain,
+                             created_by=self.user,
+                             updated_by=self.user,
+                             )
         with self.assertRaises(ValidationError):
             domain.clean()
 
     def test_valid_emaildomain(self):
         valid_email_domain = 'somedomain.com'
         domain = EmailDomain(organization=self.org,
-                             domain=valid_email_domain)
+                             domain=valid_email_domain,
+                             created_by=self.user,
+                             updated_by=self.user,
+                             )
         domain.clean()
         self.assertTrue(domain)
 
