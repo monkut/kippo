eacf7992954b2ed0c27d6f73aa3355445a88b16f
monkut <shane.cousins@gmail.com>
2018-09-05 00:10:32 +0900

    removing STATICFIELS_DIRS in settings.base.py

--- kippo/kippo/settings/base.py
+++ kippo/kippo/settings/base.py
@@ -175,27 +175,6 @@ LOGGING = {
 }
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATICFILES_DIRS = [
-    BASE_DIR / '..' / 'accounts' / 'static',
-]
-
-# NOTE:
-# >>> The following is moved to production.py
-# refer to:
-# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_LOCATION = 'static'
-#
-# # S3 Bucket Config
-# # -- for static files
-# #    (For django-storages)
-# AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME', 'kippo-staticfiles')
-# AWS_S3_CUSTOM_DOMAIN = '{}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
-# STATIC_URL = 'https://{}/'.format(AWS_S3_CUSTOM_DOMAIN)
 STATIC_URL = ''
 
 BOOTSTRAP4 = {
