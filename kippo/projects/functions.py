a9856be49a14295d4bfe3381e13a70275b442d5e
monkut <shane.cousins@gmail.com>
2019-06-13 11:53:14 +0900

    adding function for getting the csv row data of registered kippotaskstatus for a given project

--- kippo/projects/functions.py
+++ kippo/projects/functions.py
@@ -1,9 +1,11 @@
 import logging
-from typing import List
+import datetime
+from typing import List, Generator, Optional
 
 from ghorgs.managers import GithubOrganizationManager
 
 from accounts.models import KippoOrganization, KippoUser
+from tasks.models import KippoTaskStatus
 from .models import KippoProject
 
 
@@ -42,3 +44,40 @@ def collect_existing_github_projects(organization: KippoOrganization, as_user: K
         else:
             logger.debug(f'(collect_existing_github_projects) Already Exists SKIPPING: {project.name}  {project.html_url}')
     return added_projects
+
+
+def get_kippoproject_taskstatus_csv_rows(kippo_project: KippoProject, with_headers: bool = True, status_effort_date: Optional[datetime.date] = None) -> Generator:
+    """
+    Generate the current taskstaus CSV lines for a given KippoProject
+    """
+    headers = (
+        'kippo_task_id',
+        'github_issue_html_url',
+        'category',
+        'effort_date',
+        'state',
+        'estimate_days',
+        'assignee_github_login',
+        'latest_comment'
+    )
+    if with_headers:
+        yield headers
+
+    if not status_effort_date:
+        latest_kippotaskstatus = KippoTaskStatus.objects.filter(task__project=kippo_project).latest('created_datetime')
+        status_effort_date = latest_kippotaskstatus.effort_date
+
+    for taskstatus in KippoTaskStatus.objects.filter(
+            task__project=kippo_project,
+            effort_date=status_effort_date).order_by('state', 'task__category', 'task__github_issue_html_url'):
+        row = (
+            taskstatus.task.id,
+            taskstatus.task.github_issue_html_url,
+            taskstatus.task.category,
+            taskstatus.effort_date,
+            taskstatus.state,
+            taskstatus.estimate_days,
+            taskstatus.task.assignee.github_login,
+            taskstatus.comment
+        )
+        yield row
