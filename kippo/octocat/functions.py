9c20beee3c35acfe292144f892c58a2ff1d95de2
monkut <shane.cousins@gmail.com>
2019-08-04 23:34:55 +0900

    implement 'issue_comment' webhook processing

--- kippo/octocat/functions.py
+++ kippo/octocat/functions.py
@@ -11,7 +11,7 @@ from django.utils import timezone
 from ghorgs.wrappers import GithubIssue
 
 from projects.models import KippoProject
-from tasks.models import KippoTask
+from tasks.models import KippoTask, KippoTaskStatus
 from tasks.exceptions import ProjectNotFoundError
 from tasks.periodic.tasks import collect_github_project_issues, OrganizationIssueProcessor
 from accounts.models import KippoOrganization
@@ -22,49 +22,6 @@ logger = logging.getLogger(__name__)
 
 KIPPO_TESTING = strtobool(os.getenv('KIPPO_TESTING', 'False'))
 THREE_MINUTES = 3 * 60
-
-
-@task
-def process_unprocessed_events():
-    events = GithubWebhookEvent.objects.filter(state='unprocessed')
-    event_ids = []
-    if events:
-        event_ids = [e.id for e in events]
-        GithubWebhookEvent.objects.filter(id__in=event_ids).update(state='processing')
-
-        # get unique projects from events
-        organization_specific_github_projects = defaultdict(set)
-        for event in events:
-            # created, edited, moved, converted, or deleted
-            # https://developer.github.com/v3/activity/events/types/#projectcardevent
-            if event.action == 'created':
-                raise NotADirectoryError
-            elif event.action == 'edited':
-                # update title/assignees/comment, etc
-                # --> check 'changes'
-                raise NotADirectoryError
-            elif event.action == 'moved':
-                # update state to column-state
-                raise NotADirectoryError
-            elif event.action == 'converted':
-                # --> check 'changes'
-                raise NotADirectoryError
-            elif event.action == 'deleted':
-                # update status
-                raise NotADirectoryError
-
-            organization_specific_github_projects[event.related_project.organization].add(event.related_project.github_project_html_url)
-
-        if not KIPPO_TESTING:
-            logger.info('Starting Organization Github Processing....')
-            for organization, urls in organization_specific_github_projects.items():
-                logger.info(f'Webhook Triggered collect_github_project_issues() ({organization.id}) {organization.name}: {urls}')
-                *_, unhandeled_issues = collect_github_project_issues(str(organization.id), github_project_html_urls=list(urls))
-                for issue, error_args in unhandeled_issues:
-                    logger.error(f'Error handling issue({issue.html_url}): {error_args}')
-        # Update after processing
-        GithubWebhookEvent.objects.filter(id__in=event_ids).update(state='processed')
-    return event_ids
 
 
 def queue_incoming_project_card_event(organization: KippoOrganization, event_type: str, event: dict) -> GithubWebhookEvent:
@@ -109,6 +66,11 @@ class GithubWebhookProcessor:
         return issue
 
     def _process_projectcard_event(self, webhookevent: GithubWebhookEvent):
+        """
+        Process the 'project_card' event and update the related state field
+        :param webhookevent:
+        :return:
+        """
         assert webhookevent.event_type == 'project_card'
         if 'content_url' not in webhookevent.event:
             logger.warning(f'webhookevent({webhookevent.id}).event does not contain "content_url" key, SKIPPING!')
@@ -122,14 +84,16 @@ class GithubWebhookProcessor:
                 # update task state (column) for related task
                 try:
                     task = KippoTask.objects.get(github_issue_api_url=task_api_url)
-                except KippoTask.DoesNotExist:
+                except KippoTask.DoesNotExist as e:
+                    logger.exception(e)
                     state = 'error'
                     msg = f'KippoTask.github_issue_api_url={task_api_url} DoesNotExist, not updated on webhookevent "move" action'
                     logger.error(msg)
                     webhookevent.event['kippoerror'] = msg
+                    return 'error'
                 # TODO: add support for column_id to column_name
-                # create KippoTaskStatus
-                raise NotImplementedError
+                # create/update KippoTaskStatus
+
             elif webhookevent.event['action'] == 'converted':
                 # (NEW) Task create new kippo task in related project
                 try:
@@ -139,11 +103,21 @@ class GithubWebhookProcessor:
                     msg = f'KippoProject.github_project_api_url={github_project_api_url} DoesNotExist, KippoTask *NOT CREATED*!'
                     logger.error(msg)
                     webhookevent.event['kippoerror'] = msg
-                # TODO: get task details via github manager (need to add 'get_task_details()' method to ghorgs?)
-                # create KippoTask
 
-                # create KippoTaskStatus
-                raise NotImplementedError
+                # KippoTask created with 'issues' event
+                # -- update 'state' info if KippoTaskStatus exists
+                try:
+                    existing_kippotaskstatus =KippoTaskStatus.objects.filter(kippotask_set__github_issue_api_url=task_api_url).latest('created_datetime')
+                except KippoTaskStatus.DoesNotExist as e:
+                    logger.exception(e)
+                    state = 'error'
+                    msg = f'KippoTaskStatus.DoesNotExist, "state" not updated!'
+                    logger.error(msg)
+                    webhookevent.event['kippoerror'] = msg
+                    return 'errors'
+                # TODO: add support for column_id to column_name
+
+            return state
 
     def _process_issues_event(self, webhookevent: GithubWebhookEvent):
         assert webhookevent.event_type == 'issues'
@@ -165,7 +139,28 @@ class GithubWebhookProcessor:
 
     def _process_issuecomment_event(self, webhookevent: GithubWebhookEvent):
         assert webhookevent.event_type == 'issue_comment'
-        raise NotImplementedError
+        githubissue = self._load_event_to_githubissue(webhookevent.event)
+
+        # populate GithubIssue.latest_comment* fields before processing
+        # -- This enables kippo to process the GithubIssue through the standard processor
+        comment = webhookevent.event['comment']
+        githubissue.latest_comment_body = comment['body']
+        githubissue.latest_comment_created_by = comment['user']['login']
+        githubissue.latest_comment_created_at = comment['created_at']
+
+        # get related kippo project
+        # -- NOTE: Currently a GithubIssue may only be assigned to 1 Project
+        repository_api_url = githubissue.repository_url
+        candidate_projects = list(KippoProject.objects.filter(kippotask_project__github_issue_api_url__startswith=repository_api_url))
+        if len(candidate_projects) > 1:
+            raise ValueError(f'More than 1 KippoProject found for Issue.repository_url={repository_api_url}: {[p.name for p in candidate_projects]}')
+        elif len(candidate_projects) <= 0:
+            raise ProjectNotFoundError(f'KippoProject NOT found for Issue.repository_url={repository_api_url}: {[p.name for p in candidate_projects]}')
+
+        project = candidate_projects[0]
+        issue_processor = self.get_organization_issue_processor(project.organization)
+        is_new_task, new_taskstatus_entries, updated_taskstatus_entries = issue_processor.process(project, githubissue)
+        return 'processed'
 
     def process_webhook_events(self) -> int:
         unprocessed_events_for_update = GithubWebhookEvent.objects.filter(state='unprocessed').order_by('created_datetime')
