8ac62d78ee3629bbc77eda0b210288d1fd213c9f
monkut <shane.cousins@gmail.com>
2019-08-07 09:52:13 +0900

    remove unimplemented endpoint

--- kippo/projects/urls.py
+++ kippo/projects/urls.py
@@ -4,11 +4,6 @@ from . import views
 
 urlpatterns = [
     url(
-        'schedule/(?P<project_id>[0-9]+)',
-        views.view_projects_schedule,
-        name='view_projects_schedule'
-    ),
-    url(
         'overview/$',
         views.view_inprogress_projects_overview,
         name='view_inprogress_projects_overview'
