4fa82dd8928cb76cc6d9f0e38e0754854f95a0f9
monkut <shane.cousins@gmail.com>
2020-02-27 18:05:57 +0900

    :recycle: initial re-work of start/target date projects graph date Identifiers

--- kippo/tasks/charts/functions.py
+++ kippo/tasks/charts/functions.py
@@ -1,20 +1,22 @@
 import datetime
 import logging
-import time
-from typing import List
+from typing import Dict, List
 
 from bokeh.embed import components
 from bokeh.layouts import column
-from bokeh.models import ColumnDataSource, DatetimeTicker, Label, OpenURL, Span, TapTool
+from bokeh.models import ColumnDataSource, DatetimeTicker, Label, OpenURL, TapTool
+from bokeh.models.glyphs import VBar
 from bokeh.palettes import Category20
 from bokeh.plotting import figure
 from bokeh.resources import CDN
 
 logger = logging.getLogger(__name__)
 
+ONE_DAY = (3600000 * 24) - 25
+
 
 def prepare_project_schedule_chart_components(
-    project_data: dict, start_date: datetime.date, project_milestones: List[dict] = None, display_days: int = 65
+    project_data: dict, start_date: datetime.date, project_milestones: Dict[str, List[dict]] = None, display_days: int = 65
 ):
     """
     project_data =  {
@@ -22,6 +24,8 @@ def prepare_project_schedule_chart_components(
             # length of columns expected to be the same
             'project_ids': [],
             'project_names': [],
+            'project_start_dates': [],
+            'project_target_dates': [],
             'assignees': [],
             'project_assignee_grouped': [],
             'task_ids': [],
@@ -39,6 +43,8 @@ def prepare_project_schedule_chart_components(
 
     }
     """
+    if project_milestones is None:
+        project_milestones = {}
 
     # get min/max across all projects
     min_date = start_date - datetime.timedelta(days=1)
@@ -67,39 +73,29 @@ def prepare_project_schedule_chart_components(
             toolbar_location=None,
             tooltips=display_tooltips,
         )
-        # p.hbar(y="project_assignee_grouped", left="task_start_dates", right="task_end_dates", height=0.4, source=source)
-        # add assignments
-        p.square(y="project_assignee_grouped", x="task_dates", size=15, color="deepskyblue", source=source)
-        # add uncommitted
-        p.square_x(y="project_assignee_grouped", x="scheduled_dates", size=15, fill_color=None, color="deepskyblue", alpha=0.2, source=source)
-        # add unscheduled
-        p.square(y="project_assignee_grouped", x="unscheduled_dates", size=15, fill_color=None, color="deepskyblue", source=source)
-        # add uncommitted
-        p.square_x(y="project_assignee_grouped", x="uncommitted_dates", size=15, fill_color=None, color="lightgrey", alpha=0.25, source=source)
-        # add holidays
-        p.circle(y="project_assignee_grouped", x="holiday_dates", size=15, color="lightgrey", alpha=0.5, source=source)
-        # add weekends
-        p.circle(y="project_assignee_grouped", x="weekend_dates", size=15, color="lightgrey", source=source)
-        # add personal holidays
-        p.circle(y="project_assignee_grouped", x="personal_holiday_dates", size=15, color="deepskyblue", source=source)
-
-        taptool = p.select(type=TapTool)
-        taptool.callback = OpenURL(url="@task_urls")  # TODO: Finish!
-
+        project_specific_milestones = project_milestones.get(project_id, None)
         # add milestones display
-        if project_milestones and project_id in project_milestones:
-            milestone_count = len(project_milestones[project_id])
+        label_y = -1
+        label_x_offset = 15
+        if project_specific_milestones:
+            milestone_count = len(project_specific_milestones)
             color_count = 3
             if milestone_count > 3:
                 color_count = milestone_count
             color_palette = Category20[color_count]  # assumes milestones will not be > 20
-            for idx, milestone_info in enumerate(project_milestones[project_id]):
-                # bokeh requires this time format for display
-                milestone_date = time.mktime(milestone_info["target_date"].timetuple()) * 1000
-                milestone = Span(location=milestone_date, dimension="height", line_color=color_palette[idx], line_dash="dashed", line_width=3)
-                p.add_layout(milestone)
+            for idx, milestone_info in enumerate(project_specific_milestones):
+                milestone_date = milestone_info["target_date"]
+                glyph = VBar(x=milestone_date, top=calculated_plot_height, bottom=-50, width=ONE_DAY, line_color=color_palette[idx], fill_color=None)
+                p.add_glyph(source, glyph)
+
                 label = Label(
-                    x=milestone_date, x_offset=5, y=0, y_offset=1, text=milestone_info["title"], text_font_style="italic", text_font_size="8pt"
+                    x=milestone_date,
+                    x_offset=label_x_offset,
+                    y=label_y,
+                    y_offset=1,
+                    text=milestone_info["title"],
+                    text_font_style="italic",
+                    text_font_size="8pt",
                 )
                 p.add_layout(label)
 
@@ -107,23 +103,46 @@ def prepare_project_schedule_chart_components(
             project_start_date = data["project_start_dates"][0]
             if project_start_date > min_date:
                 logger.debug(f"project_start_date: {project_start_date}")
-                project_start_date = time.mktime(project_start_date.timetuple()) * 1000  # bokeh requires this time format for display
-                project_start = Span(location=project_start_date, dimension="height", line_color="green", line_dash="solid", line_width=2)
-                p.add_layout(project_start)
+                glyph = VBar(x=project_start_date, top=calculated_plot_height, bottom=-50, width=ONE_DAY, line_color="green", fill_color=None)
+                p.add_glyph(source, glyph)
                 project_start_date_label = Label(
-                    x=project_start_date, x_offset=-15, y=0, y_offset=1, text="Start", text_font_style="italic", text_font_size="8pt"
+                    x=project_start_date, x_offset=label_x_offset, y=label_y, y_offset=0, text="Start", text_font_style="italic", text_font_size="8pt"
                 )
                 p.add_layout(project_start_date_label)
         if "project_target_dates" in data and data["project_target_dates"]:  # will be empty if not defined
             project_target_date = data["project_target_dates"][0]
             logger.debug(f"project_target_date: {project_target_date}")
-            project_end_date = time.mktime(project_target_date.timetuple()) * 1000  # bokeh requires this time format for display
-            project_end = Span(location=project_end_date, dimension="height", line_color="red", line_dash="solid", line_width=5)
-            p.add_layout(project_end)
+            glyph = VBar(x=project_target_date, top=calculated_plot_height, bottom=-50, width=ONE_DAY, line_color="red", fill_color=None)
+            p.add_glyph(source, glyph)
+            # p.vbar(x=project_target_date, top=calculated_plot_height, bottom=-50, width=.5, line_color='red', fill_color=None)
             project_target_date_label = Label(
-                x=project_target_date, x_offset=5, y=0, y_offset=1, text="Target", text_font_style="italic", text_font_size="8pt"
+                x=project_target_date, x_offset=label_x_offset, y=label_y, y_offset=0, text="Target", text_font_style="italic", text_font_size="8pt"
             )
             p.add_layout(project_target_date_label)
+
+        # add assignments
+        p.square(y="project_assignee_grouped", x="task_dates", size=15, color="deepskyblue", source=source)  # , legend_label="Scheduled Task")
+        # add uncommitted
+        p.square_x(y="project_assignee_grouped", x="scheduled_dates", size=15, fill_color=None, color="deepskyblue", alpha=0.2, source=source)  # ,
+        # legend_label="Other Project Scheduled Task")
+        # add unscheduled
+        p.square(
+            y="project_assignee_grouped", x="unscheduled_dates", size=15, fill_color=None, color="deepskyblue", source=source
+        )  # , legend_label="Unscheduled")
+        # add uncommitted
+        p.square_x(y="project_assignee_grouped", x="uncommitted_dates", size=15, fill_color=None, color="lightgrey", alpha=0.25, source=source)
+        # legend_label="Uncommitted")
+        # add holidays
+        p.circle(y="project_assignee_grouped", x="holiday_dates", size=15, color="lightgrey", alpha=0.5, source=source)  # , legend_label="Holiday")
+        # add weekends
+        p.circle(y="project_assignee_grouped", x="weekend_dates", size=15, color="lightgrey", source=source)  # , legend_label="Weekend")
+        # add personal holidays
+        p.circle(
+            y="project_assignee_grouped", x="personal_holiday_dates", size=15, color="deepskyblue", source=source
+        )  # , legend_label="Personal Holiday")
+
+        taptool = p.select(type=TapTool)
+        taptool.callback = OpenURL(url="@task_urls")  # TODO: Finish!
 
         p.xaxis.ticker = DatetimeTicker()
         p.yaxis.group_label_orientation = "horizontal"
