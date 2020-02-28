ab10e19b0bd15dc67afc51bdb466e11a559e5d47
monkut <shane.cousins@gmail.com>
2020-02-28 11:02:25 +0900

    :wrench: adding KippoTaskStatusAdmin to use prettyprintJsonWidget - Adjust widget width

--- kippo/common/admin.py
+++ kippo/common/admin.py
@@ -142,5 +142,5 @@ class PrettyJSONWidget(widgets.Textarea):
         # these lines will try to adjust size of TextArea to fit to content
         row_lengths = [len(r) for r in value.split("\n")]
         self.attrs["rows"] = min(max(len(row_lengths) + 2, 10), 30)
-        self.attrs["cols"] = min(max(max(row_lengths) + 2, 100), 120)
+        self.attrs["cols"] = min(max(max(row_lengths) + 2, 50), 120)
         return value
--- kippo/tasks/admin.py
+++ kippo/tasks/admin.py
@@ -1,84 +1,76 @@
+from accounts.admin import UserCreatedBaseModelAdmin
+from common.admin import OrganizationTaskQuerysetModelAdminMixin, PrettyJSONWidget
 from django.contrib import admin
+from django.contrib.postgres.fields import JSONField
 from django.utils.html import format_html
 from django.utils.translation import ugettext_lazy as _
-
-from accounts.admin import UserCreatedBaseModelAdmin
-from common.admin import OrganizationTaskQuerysetModelAdminMixin
 
 from .models import KippoTask, KippoTaskStatus
 
 
 class KippoTaskAdmin(OrganizationTaskQuerysetModelAdminMixin, UserCreatedBaseModelAdmin):
     list_display = (
-        'title',
-        'category',
-        'get_kippoproject_name',
-        'get_kippomilestone_title',
-        'get_assignee_display_name',
-        'get_github_issue_html_url',
-        'github_issue_api_url',
+        "title",
+        "category",
+        "get_kippoproject_name",
+        "get_kippomilestone_title",
+        "get_assignee_display_name",
+        "get_github_issue_html_url",
+        "github_issue_api_url",
     )
-    search_fields = (
-        'title',
-    )
+    search_fields = ("title",)
 
     def get_kippoproject_name(self, obj):
-        result = ''
+        result = ""
         if obj.project and obj.project.name:
             result = obj.project.name
         return result
-    get_kippoproject_name.short_description = 'KippoProject'
+
+    get_kippoproject_name.short_description = "KippoProject"
 
     def get_kippomilestone_title(self, obj):
-        result = ''
+        result = ""
         if obj.milestone and obj.milestone.title:
             result = obj.milestone.title
         return result
-    get_kippomilestone_title.short_description = 'KippoMilestone'
+
+    get_kippomilestone_title.short_description = "KippoMilestone"
 
     def get_assignee_display_name(self, obj):
-        result = ''
+        result = ""
         if obj and obj.assignee:
             result = obj.assignee.display_name
         return result
-    get_assignee_display_name.short_description = 'Assignee'
+
+    get_assignee_display_name.short_description = "Assignee"
 
     def get_github_issue_html_url(self, obj):
-        url = ''
+        url = ""
         if obj.github_issue_html_url:
             url = format_html('<a href="{url}">{url}</a>', url=obj.github_issue_html_url)
         return url
-    get_github_issue_html_url.short_description = _('Github Issue URL')
+
+    get_github_issue_html_url.short_description = _("Github Issue URL")
 
 
 class KippoTaskStatusAdmin(UserCreatedBaseModelAdmin):
-    list_display = (
-        'display_name',
-        'effort_date',
-        'state',
-        'get_assignee',
-        'minimum_estimate_days',
-        'estimate_days',
-        'maximum_estimate_days',
-    )
-    search_fields = (
-        'task__assignee__github_login',
-        'task__github_issue_html_url',
-        'task__title',
-    )
+    list_display = ("display_name", "effort_date", "state", "get_assignee", "minimum_estimate_days", "estimate_days", "maximum_estimate_days")
+    search_fields = ("task__assignee__github_login", "task__github_issue_html_url", "task__title")
+    formfield_overrides = {JSONField: {"widget": PrettyJSONWidget}}
 
     def get_assignee(self, obj=None) -> str:
-        result = ''
+        result = ""
         if obj and obj.task.assignee:
             result = obj.task.assignee.github_login
         return result
-    get_assignee.short_description = 'ASSIGNEE'
+
+    get_assignee.short_description = "ASSIGNEE"
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         if request.user.is_superuser:
             return qs
-        return qs.filter(task__project__organization__in=request.user.organizations).order_by('task__project__organization').distinct()
+        return qs.filter(task__project__organization__in=request.user.organizations).order_by("task__project__organization").distinct()
 
 
 admin.site.register(KippoTask, KippoTaskAdmin)
