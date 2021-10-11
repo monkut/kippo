c87b1570df9cc739db459a07d9085215ebbd72d0
monkut <shane.cousins@gmail.com>
2021-10-11 15:55:06 +0900

    :pencil: add 'github_login' to useradmin

--- kippo/accounts/admin.py
+++ kippo/accounts/admin.py
@@ -146,7 +146,7 @@ class KippoUserAdmin(AllowIsStaffReadonlyMixin, OrganizationQuerysetModelAdminMi
     # limit displayed fields
     fieldsets = (
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "holiday_country")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "github_login", "email", "holiday_country")}),
         (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser")}),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
